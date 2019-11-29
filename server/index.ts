9bbd9c5191e76554d885540e588971ed5af47d5c
Jake Ginnivan <jakeginnivan@Jakes-MacBook-Pro.local>
2019-11-29 14:18:14 +0800

    Added header for allowing all methods via cors

--- server/index.ts
+++ server/index.ts
@@ -255,6 +255,7 @@ const optionsResponse = new aws.lambda.CallbackFunction('live-blog-option-respon
             statusCode: 200,
             headers: {
                 'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
                 'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
             },
             body: ''
