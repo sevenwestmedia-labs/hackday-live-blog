49434c53da19ef2e1b210be75408b6b4ae6f4530
Jake Ginnivan <jakeginnivan@Jakes-MacBook-Pro.local>
2019-11-29 14:37:50 +0800

    Add columns

--- client/src/App.js
+++ client/src/App.js
@@ -1,184 +1,201 @@
-import React from 'react';
-import './App.css';
-import ReactQuill from 'react-quill';
-import Parser from 'html-react-parser';
+import React from 'react'
+import './App.css'
+import { css } from 'emotion'
+import ReactQuill from 'react-quill'
+import Parser from 'html-react-parser'
 //import {} from 'swm-cue-parser/dist/cjs/block';
 
-const restUri = 'https://4odo7ux4lc.execute-api.ap-southeast-2.amazonaws.com/stage';
-const blogId = 1;
+const restUri = 'https://4odo7ux4lc.execute-api.ap-southeast-2.amazonaws.com/stage'
+const blogId = 1
 
 class StatusBar extends React.Component {
-  render() {
-    var status
-    const post = this.props.currentPost
-    if (post) {
-      status = 'Editiing post ' + post.post + ' published at ' + post.posted
-    } else {
-      status = 'Add a new post'
+    render() {
+        var status
+        const post = this.props.currentPost
+        if (post) {
+            status = 'Editiing post ' + post.post + ' published at ' + post.posted
+        } else {
+            status = 'Add a new post'
+        }
+        return <div>{status}</div>
     }
-    return <div>{status}</div>
-  }
 }
 
 class PostList extends React.Component {
-  render() {
-    const editPost = this.props.editPost
-    const posts = this.props.posts.map(function (item,index) {
-      return (
-        <div key={index}>
-          <BlogPost index={index} post={item} editPost={editPost}/>
-        </div>
-      )
-    })
-    return <div>{posts}</div>
-  }
+    render() {
+        const editPost = this.props.editPost
+        const posts = this.props.posts.map(function(item, index) {
+            return (
+                <div key={index}>
+                    <BlogPost index={index} post={item} editPost={editPost} />
+                </div>
+            )
+        })
+        return <div>{posts}</div>
+    }
 }
 
 class BlogPost extends React.Component {
-  render() {
-    const post = this.props.post
-    const index = this.props.index
-    const editPost = this.props.editPost
-    return (
-      <div id={index}>
-        {Parser(post.blocks[0].text)}
-        <button onClick={() => {
-          editPost(index)
-          }}>edit</button>
-      </div>
-    )
-  }
+    render() {
+        const post = this.props.post
+        const index = this.props.index
+        const editPost = this.props.editPost
+        return (
+            <div id={index}>
+                {Parser(post.blocks[0].text)}
+                <button
+                    onClick={() => {
+                        editPost(index)
+                    }}
+                >
+                    edit
+                </button>
+            </div>
+        )
+    }
 }
 
 class QuestionList extends React.Component {
-  render() {
-    const questions = this.props.questions.map(function (item) {
-      return (
-        <div>
-          <Question key={item.id} question={item}/>
-        </div>
-      )
-    })
-    return <div>{questions}</div>
-  }
+    render() {
+        const questions = this.props.questions.map(function(item) {
+            return (
+                <div>
+                    <Question key={item.id} question={item} />
+                </div>
+            )
+        })
+        return <div>{questions}</div>
+    }
 }
 
 class Question extends React.Component {
-  render() {
-    return (
-      <div id={this.props.key}>
-        {Parser(this.props.question.content)}
-        <button onClick={() => {
-          this.props.editPost(this.props.id)
-          }}>Answer</button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div id={this.props.key}>
+                {Parser(this.props.question.content)}
+                <button
+                    onClick={() => {
+                        this.props.editPost(this.props.id)
+                    }}
+                >
+                    Answer
+                </button>
+            </div>
+        )
+    }
 }
 
-function stripHtml(html)
-{
-   var tmp = document.createElement("DIV");
-   tmp.innerHTML = html;
-   return tmp.textContent || tmp.innerText || "";
+function stripHtml(html) {
+    var tmp = document.createElement('DIV')
+    tmp.innerHTML = html
+    return tmp.textContent || tmp.innerText || ''
 }
 
 async function submitPost(text) {
-  const url = restUri + '/posts/' + blogId
-  const stripped = stripHtml(text);
-  const post = {
-    posted: new Date().toISOString(),
-    blocks: [
-      {
-        kind: 'text',
-        text: stripped,
-        intentions: []
-      }
-    ],
-    author: 'bob'
-  }
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(post),
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+    const url = restUri + '/posts/' + blogId
+    const stripped = stripHtml(text)
+    const post = {
+        posted: new Date().toISOString(),
+        blocks: [
+            {
+                kind: 'text',
+                text: stripped,
+                intentions: []
+            }
+        ],
+        author: 'bob'
     }
-  })
-  const responseBody = await response.json()
-  console.log(response,responseBody);
-  post.post = responseBody.postId
-  return post
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    const responseBody = await response.json()
+    console.log(response, responseBody)
+    post.post = responseBody.postId
+    return post
 }
 
 async function submitPut(post) {
-  console.log(post)
-  const url = restUri + `/posts/${blogId}/${post.post}`
-  const response = await fetch(url, {
-    method: 'PUT',
-    body: JSON.stringify(post),
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-  return post
+    console.log(post)
+    const url = restUri + `/posts/${blogId}/${post.post}`
+    const response = await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(post),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    return post
 }
 
 const MyComponent = () => {
-  const [text, setText] = React.useState('')
-  const [posts, setPosts] = React.useState([])
-  const [questions, setQuestions] = React.useState([])
-  const [currentPostIndex, setCurrentPostIndex] = React.useState()
-  const [currentPost, setCurrentPost] = React.useState()
-  const [currentQuestion, setCurrentQuestion] = React.useState()
+    const [text, setText] = React.useState('')
+    const [posts, setPosts] = React.useState([])
+    const [questions, setQuestions] = React.useState([])
+    const [currentPostIndex, setCurrentPostIndex] = React.useState()
+    const [currentPost, setCurrentPost] = React.useState()
+    const [currentQuestion, setCurrentQuestion] = React.useState()
 
     return (
-      <div>
-        <StatusBar currentPost={currentPost} currentQuestion={currentQuestion}/>
-        <ReactQuill value={text}
-                    onChange={e => setText(e)} />
-        <button onClick={async () => {
-            if(!currentPost) {
-              const newItem = await submitPost(text)
-              setPosts([...posts, newItem])
-              setCurrentPost(null)
-              setText('')
-            } else {
-              const newPosts = posts.slice()
-              console.log(currentPostIndex)
-              newPosts[currentPostIndex].blocks[0].text = stripHtml(text)
-              setPosts(newPosts)
-              await submitPut(newPosts[currentPostIndex])
-              setCurrentPostIndex(null)
-              setCurrentPost(null)
-              setText('')
-            }
-        }}>{currentPost ? 'Edit' : 'Submit'}</button>
-          <button onClick={async () => {
-              setCurrentPost(null)
-              setText('')
-        }}>Cancel</button>
-        <div id="posts">
-          <PostList posts={posts}
-            editPost={(index) => {
-              const currentPost = posts[index]
-              setCurrentPost(currentPost)
-              setCurrentPostIndex(index)
-              setText(currentPost.blocks[0].text)
-            }
-          }
-          />
-          <QuestionList questions={questions}/>
+        <div className={css({ display: 'grid', gridTemplateColumns: '2fr 1fr' })}>
+            <div>
+                <StatusBar currentPost={currentPost} currentQuestion={currentQuestion} />
+                <ReactQuill value={text} onChange={e => setText(e)} />
+                <button
+                    onClick={async () => {
+                        if (!currentPost) {
+                            const newItem = await submitPost(text)
+                            setPosts([...posts, newItem])
+                            setCurrentPost(null)
+                            setText('')
+                        } else {
+                            const newPosts = posts.slice()
+                            console.log(currentPostIndex)
+                            newPosts[currentPostIndex].blocks[0].text = stripHtml(text)
+                            setPosts(newPosts)
+                            await submitPut(newPosts[currentPostIndex])
+                            setCurrentPostIndex(null)
+                            setCurrentPost(null)
+                            setText('')
+                        }
+                    }}
+                >
+                    {currentPost ? 'Edit' : 'Submit'}
+                </button>
+                <button
+                    onClick={async () => {
+                        setCurrentPost(null)
+                        setText('')
+                    }}
+                >
+                    Cancel
+                </button>
+                <div id="posts">
+                    <PostList
+                        posts={posts}
+                        editPost={index => {
+                            const currentPost = posts[index]
+                            setCurrentPost(currentPost)
+                            setCurrentPostIndex(index)
+                            setText(currentPost.blocks[0].text)
+                        }}
+                    />
+                </div>
+            </div>
+            <div>
+                <QuestionList questions={questions} />
+            </div>
         </div>
-      </div>
     )
 }
 
 function App() {
-  return (
-    <MyComponent/>
-  );
+    return <MyComponent />
 }
 
-export default App;
+export default App
